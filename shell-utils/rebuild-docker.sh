1754c5f843fb174767cf0ce89072545e0ba75df9
Yacine Kherbache <yacineKherbache@yaci.fr>
2023-07-17 01:10:01 +0200

    feat: understood how to use ansible, now i need to configure my own setup of it.

---
+++ shell-utils/rebuild-docker.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# This script is used to rebuild the docker image
+
+docker build -t ansible-course .
---
+++ shell-utils/run-ansible.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# This script is used to run ansible-playbook to configure the docker host
+
+ansible-playbook fem.yml
